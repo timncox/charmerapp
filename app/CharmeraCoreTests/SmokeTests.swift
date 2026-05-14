95c0720096fd7a5acb150375aac2adfc464c356e
timncox <timcox@gmail.com>
2026-05-14 18:35:17 -0400

    Revert "test: add CharmeraCoreTests target"
    This reverts commit ad0c6df71f88297916735717c12286768e25c553.

--- app/CharmeraCoreTests/SmokeTests.swift
+++
@@ -1,8 +0,0 @@
-import Testing
-@testable import CharmeraCore
-
-@Suite struct SmokeTests {
-    @Test func charmeraCoreIsImportable() {
-        #expect(Config.repoName == "charmera-gallery")
-    }
-}
